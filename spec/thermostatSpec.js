6e8c91667232d5ee0eee50479f901e041d890df5
Lucian Mot <lucian.i.mot@gmail.com>
2019-06-28 10:30:06 +0100

    almost finished 1st chapter

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -23,9 +23,53 @@ describe('Thermostat', function(){
   })
 
   it('has minimum of 10 degrees', function() {
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 11; i++) {
       thermostat.down();
     }
     expect(thermostat.getCurrentTemperature()).toEqual(10);
   });
+
+  it('has power saving mode on by default', function() {
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+
+  it('can switch PSM off', function() {
+    thermostat.switchPowerSavingModeOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+  });
+
+  it('can switch PSM back on', function() {
+    thermostat.switchPowerSavingModeOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+    thermostat.switchPowerSavingModeOn();
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+
+  describe('when power saving mode is on', function() {
+  it('has a maximum temperature of 25 degrees', function() {
+    for (var i = 0; i < 6; i++) {
+      thermostat.up();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(25);
+    });
+  });
+
+  describe('when power saving mode is off', function() {
+    it('has a maximum temperature of 32 degrees', function() {
+      thermostat.switchPowerSavingModeOff();
+      for (var i = 0; i < 13; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(32);
+    });
+  });
+
+  it('can be reset to the default temperature', function() {
+    for (var i = 0; i < 6; i++) {
+      thermostat.up();
+    }
+    thermostat.resetTemperature();
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,28 +1,55 @@
 'use strict';
 
 function Thermostat() {
-  // if you would like to target without using this just make it a variable
-  // var temp = this.temperature
   this.MINIMUM_TEMPERATURE = 10;
+  this.MAX_LIMIT_PSM_ON = 25;
+  this.MAX_LIMIT_PSM_OFF = 32;
   this.temperature = 20;
-  this.up = function() {
-     return this.temperature += 1;
+  this.powerSavingMode = true;
+
+  this.isMinimumTemperature = function() {
+    return this.temperature === this.MINIMUM_TEMPERATURE;
   };
+
+  this.isMaximumTemperature = function() {
+    if (this.isPowerSavingModeOn() === false) {
+      return this.temperature === this.MAX_LIMIT_PSM_OFF;
+    }
+      return this.temperature === this.MAX_LIMIT_PSM_ON;
+  }
+
+  this.up = function() {
+    if (this.isMaximumTemperature()) {
+      return;
+    }
+    this.temperature += 1;
+  };
+
   this.down = function() {
+    if (this.isMinimumTemperature()) {
+      return;
+    }
      return this.temperature -= 1;
   };
+
+  this.isPowerSavingModeOn = function() {
+    return this.powerSavingMode === true;
+  };
+
+  this.switchPowerSavingModeOff = function() {
+    this.powerSavingMode = false;
+  }
+
+  this.switchPowerSavingModeOn = function() {
+    this.powerSavingMode = true;
+  }
+
   this.getCurrentTemperature = function() {
    return this.temperature;
   };
-};
 
-Thermostat.prototype.isMinimumTemperature = function() {
-  return this.temperature === this.MINIMUM_TEMPERATURE;
-};
-
-Thermostat.prototype.isMinimumTemperature = function() {
-  if (this.isMinimumTemperature()) {
-    return;
+  this.resetTemperature = function() {
+    this.temperature = 20;
   }
-  this.temperature -= 1;
-}
+
+};
